a193eae951244da1236bf52668fe15e49973ca78
ftsuda-senac <ftsuda.senac@gmail.com>
2020-09-28 20:56:14 -0300

    Apresentado formulario preenchido

--- exemplos-spring/src/main/java/br/senac/tads/dsw/exemplosspring/FormularioController.java
+++ exemplos-spring/src/main/java/br/senac/tads/dsw/exemplosspring/FormularioController.java
@@ -5,6 +5,8 @@
  */
 package br.senac.tads.dsw.exemplosspring;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -18,7 +20,30 @@ public class FormularioController {
 
     @GetMapping
     public ModelAndView mostrarForm() {
-        return new ModelAndView("formulario");
+        ModelAndView mv = new ModelAndView("formulario");
+        mv.addObject("dados", new Formulario());
+        return mv;
+    }
+
+    @GetMapping("/preenchido")
+    public ModelAndView mostrarFormPreenchido() {
+        Formulario formulario = new Formulario();
+        formulario.setNome("Seu Madruga");
+        formulario.setEmail("madruga@teste.com.br");
+        formulario.setTelefone("11 98765-1122");
+        formulario.setSenha("abcd1234");
+        formulario.setRepetirSenha("abcd1234");
+        formulario.setDescricao("Descrição Seu Madruga");
+        formulario.setNumero(72);
+        formulario.setPeso(new BigDecimal(72));
+        formulario.setAltura(new BigDecimal(1.72));
+        formulario.setDataNascimento(LocalDate.of(1972, 10, 10));
+        formulario.setSexo(1);
+        formulario.setInteresses(new String[]{"Gastronomia", "Esportes"});
+
+        ModelAndView mv = new ModelAndView("formulario");
+        mv.addObject("dados", formulario);
+        return mv;
     }
 
     @PostMapping
@@ -27,5 +52,5 @@ public class FormularioController {
         mv.addObject("dados", formulario);
         return mv;
     }
-    
+
 }
